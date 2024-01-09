e549544cd01013c5d4da66f03f5087d0711c1090
Anton Karlsson <anton93karlsson@gmail.com>
2024-01-09 19:28:27 +0100

    Format file

--- nvim/lua/plugin.lua
+++ nvim/lua/plugin.lua
@@ -1,85 +1,84 @@
 require('packer').startup(function()
-  -- Packer manages itself
-  use 'wbthomason/packer.nvim'
+    -- Packer manages itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.3',
--- or                            , branch = '0.1.x',
-  requires = { {'nvim-lua/plenary.nvim'} }
-}
-  -- LSP
-  use 'neovim/nvim-lspconfig'
-
-  use {
-  "nvim-neotest/neotest",
-  "nvim-neotest/neotest-python",
-  requires = {
-    "nvim-lua/plenary.nvim",
-    "antoinemadec/FixCursorHold.nvim",
-    "nvim-treesitter/nvim-treesitter"
-  }
-}
-
-  use 'folke/neodev.nvim'
-
-  use 'lewis6991/gitsigns.nvim'
-
-  use {'hrsh7th/nvim-cmp',
-     requires = {
-          'hrsh7th/cmp-nvim-lsp',
-          'hrsh7th/cmp-buffer',
-          'hrsh7th/cmp-path',
-          'hrsh7th/cmp-cmdline'
-      }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.3',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
-  -- Treesitter for better syntax highlighting
-  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+    -- LSP
+    use 'neovim/nvim-lspconfig'
 
-  -- REPL integration (Conjure)
-  use 'Olical/conjure'
-  use {'eraserhd/parinfer-rust', run='! cargo build --release'}
-
-  use 'mg979/vim-visual-multi'
-
-  use {
-  "folke/which-key.nvim",
-  config = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-    require("which-key").setup {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-      triggers_nowait = {
-          -- marks
-          " ",
-          "`",
-          "'",
-          "g`",
-          "g'",
-          -- registers
-          '"',
-          "<c-r>",
-          -- spelling
-          "z=",
-      },
-      triggers_blacklist = {
-          -- list of mode / prefixes that should never be hooked by WhichKey
-          -- this is mostly relevant for keymaps that start with a native binding
-          i = { "j", "k" },
-          v = { "j", "k" },
-      },
+    use {
+        "nvim-neotest/neotest",
+        "nvim-neotest/neotest-python",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "antoinemadec/FixCursorHold.nvim",
+            "nvim-treesitter/nvim-treesitter"
+        }
     }
-  end
-  }
 
-  use 'ray-x/lsp_signature.nvim'
-  use 'folke/zen-mode.nvim'
-  use 'tpope/vim-surround'
-  use 'morhetz/gruvbox'
-  use 'wlangstroth/vim-racket'
+    use 'folke/neodev.nvim'
 
-  use 'kyazdani42/nvim-web-devicons'
-  use 'hoob3rt/lualine.nvim'
+    use 'lewis6991/gitsigns.nvim'
 
+    use { 'hrsh7th/nvim-cmp',
+        requires = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline'
+        }
+    }
+    -- Treesitter for better syntax highlighting
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+    -- REPL integration (Conjure)
+    use 'Olical/conjure'
+    use { 'eraserhd/parinfer-rust', run = '! cargo build --release' }
+
+    use 'mg979/vim-visual-multi'
+
+    use {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+                triggers_nowait = {
+                    -- marks
+                    " ",
+                    "`",
+                    "'",
+                    "g`",
+                    "g'",
+                    -- registers
+                    '"',
+                    "<c-r>",
+                    -- spelling
+                    "z=",
+                },
+                triggers_blacklist = {
+                    -- list of mode / prefixes that should never be hooked by WhichKey
+                    -- this is mostly relevant for keymaps that start with a native binding
+                    i = { "j", "k" },
+                    v = { "j", "k" },
+                },
+            }
+        end
+    }
+
+    use 'ray-x/lsp_signature.nvim'
+    use 'folke/zen-mode.nvim'
+    use 'tpope/vim-surround'
+    use 'morhetz/gruvbox'
+    use 'wlangstroth/vim-racket'
+
+    use 'kyazdani42/nvim-web-devicons'
+    use 'hoob3rt/lualine.nvim'
 end)
