c7ff9fc21e2e8950db035af346c92dbf15c5f280
Anton Karlsson <anton93karlsson@gmail.com>
2024-02-07 19:06:54 +0100

    refactor

--- nvim/lua/plugin.lua
+++ nvim/lua/plugin.lua
@@ -4,10 +4,9 @@ require('packer').startup(function()
 
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.3',
-        -- or                            , branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
-    -- LSP
+
     use 'neovim/nvim-lspconfig'
 
     use "mfussenegger/nvim-dap"
