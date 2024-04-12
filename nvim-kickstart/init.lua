483ab6a94fd3d846825cce505a1cba9989f6b2f5
Anton Karlsson <anton.karlsson@fortnox.se>
2024-04-12 17:08:44 +0200

    Use default path in telescope add pytight withouth the type errors

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -439,7 +439,6 @@ require('telescope').setup {
         ['<C-d>'] = false,
       },
     },
-    path_display = "smart",
   },
 }
 
@@ -637,7 +636,7 @@ require('which-key').register {
   ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
   ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
   ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+  ['<leader>h'] = { name = 'Git [H]unk and Harpoon', _ = 'which_key_ignore' },
   ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
   ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
   ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
@@ -666,7 +665,13 @@ require('mason-lspconfig').setup()
 local servers = {
   clangd = {},
   -- gopls = {},
-  -- pyright = {},
+  pyright = {
+    python = {
+      analysis = {
+        typeCheckingMode = "off",
+      },
+    },
+  },
   -- rust_analyzer = {},
   -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
