cae3b588cac27e90ebb43dd4df890fe4cc16faf1
Anton Karlsson <anton.karlsson@fortnox.se>
2024-04-25 15:16:18 +0200

    Remove some harpoon keybindings

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -793,10 +793,6 @@ vim.keymap.set("n", "<A-1>", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<A-2>", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<A-3>", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<A-4>", function() harpoon:list():select(4) end)
--- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
-
 
 -- [[ Configure Oil ]]
 local oil = require "oil"
