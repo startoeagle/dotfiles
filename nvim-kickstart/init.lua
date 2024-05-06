11a53813b5d82df41feeac2bddc62af461c0642b
Anton Karlsson <anton.karlsson@fortnox.se>
2024-05-06 09:20:34 +0200

    Remove config parameters for linters and formatters

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -837,7 +837,6 @@ null_ls.setup({
 })
 
 require('lint').linters_by_ft = {
-  python = { 'flakeheaven', 'pyre' }
 }
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
