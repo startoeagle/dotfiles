3a6faae56dd2fa165c1e8a751a4faffa317e6aa3
Anton Karlsson <anton93karlsson@gmail.com>
2024-05-25 10:53:49 +0200

    Add tree sitter playground

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -249,6 +249,10 @@ require('lazy').setup({
     },
     build = ':TSUpdate',
   },
+  {
+    'nvim-treesitter/playground',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' }
+  },
 
   'github/copilot.vim',
 
@@ -565,6 +569,24 @@ vim.defer_fn(function()
         },
       },
     },
+    playground = {
+      enable = true,
+      disable = {},
+      updatetime = 25,       -- Debounced time for highlighting nodes in the playground from source code
+      persist_queries = false, -- Whether the query persists across vim sessions
+      keybindings = {
+        toggle_query_editor = 'o',
+        toggle_hl_groups = 'i',
+        toggle_injected_languages = 't',
+        toggle_anonymous_nodes = 'a',
+        toggle_language_display = 'I',
+        focus_language = 'f',
+        unfocus_language = 'F',
+        update = 'R',
+        goto_node = '<cr>',
+        show_help = '?',
+      },
+    }
   }
 end, 0)
 
