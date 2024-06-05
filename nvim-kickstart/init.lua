3737478a5865fdb2f1bd74bf44e4466b3eaa12e9
Anton Karlsson <anton93karlsson@gmail.com>
2024-06-05 17:51:38 +0200

    Formatting

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -790,21 +790,8 @@ cmp.setup {
     }, {
       { name = 'cmdline' }
     }),
-    cmp.setup.cmdline('/', {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = 'buffer' }
-      }
-    }),
-    cmp.setup.cmdline(':', {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = 'path' }
-      }, {
-        { name = 'cmdline' }
-      }),
-      matching = { disallow_symbol_nonprefix_matching = false }
-    })
+    matching = { disallow_symbol_nonprefix_matching = false }
+  })
 }
 
 -- [[ Configure harpoon ]]
