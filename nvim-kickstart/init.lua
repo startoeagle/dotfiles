e6f14204bb17b94d28d23a6d9ec3b081843898c3
Anton Karlsson <anton.karlsson@fortnox.se>
2024-10-13 07:57:40 +0200

    Add keyboard config for Oil.nvim

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -818,6 +818,27 @@ vim.keymap.set("n", "<A-4>", function() harpoon:list():select(4) end)
 
 -- [[ Configure Oil ]]
 local oil = require "oil"
+oil.setup({
+  keymaps = {
+    ["g?"] = "actions.show_help",
+    ["<CR>"] = "actions.select",
+    ["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+    ["<C-h>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
+    ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+    ["<C-p>"] = "actions.preview",
+    ["<C-c>"] = "actions.close",
+    ["<C-l>"] = "actions.refresh",
+    ["-"] = "actions.parent",
+    ["_"] = "actions.open_cwd",
+    ["`"] = "actions.cd",
+    ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
+    ["gs"] = "actions.change_sort",
+    ["gx"] = "actions.open_external",
+    ["g."] = "actions.toggle_hidden",
+    ["g\\"] = "actions.toggle_trash",
+  },
+})
+
 vim.keymap.set("n", "<leader>oh", oil.open, { desc = '[O]il [H]ere (current directory of the file)' })
 
 -- [[ Configure Neotest ]]
