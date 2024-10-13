ae4a2f0bbb9458277aacf08c373e00f91a379954
Anton Karlsson <anton.karlsson@fortnox.se>
2024-10-13 13:41:31 +0200

    feat: integrate Neogit plugin with dependencies
    Added Neogit plugin to enhance Git operations within Neovim. Included
    necessary dependencies such as plenary.nvim and diffview.nvim. Also
    added optional pickers like telescope.nvim, fzf-lua, and mini.pick.
    Configured Neogit with default settings for immediate use.

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -34,6 +34,22 @@ require('lazy').setup({
 
   -- Git related plugins
   'tpope/vim-fugitive',
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",  -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua",              -- optional
+      "echasnovski/mini.pick",         -- optional
+    },
+    config = function()
+      local neogit = require("neogit")
+      neogit.setup({})
+    end
+  },
   'tpope/vim-rhubarb',
 
   -- Detect tabstop and shiftwidth automatically
