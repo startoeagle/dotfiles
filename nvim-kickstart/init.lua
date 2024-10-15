ee170974768c6501daad40735e84b8de9a73e5d1
Anton Karlsson <anton.karlsson@fortnox.se>
2024-10-15 18:29:21 +0200

    feat: update which-key registration method
    Updated the which-key registration method from `register` to `add` for
    better readability and maintainability. This change affects the key
    bindings for various functionalities such as Code and Copilot, Document,
    Git, Git Hunk and Harpoon, Rename, Search, Toggle, Workspace, and Oil.
    Also updated the visual mode registration for Git Hunk.

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -774,23 +774,23 @@ local on_attach = function(_, bufnr)
 end
 
 -- document existing key chains
-require('which-key').register {
-  ['<leader>c'] = { name = '[C]ode and Copilot', _ = 'which_key_ignore' },
-  ['<leader>cp'] = { name = '[C]o[p]ilot', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'Git [H]unk and Harpoon', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-  ['<leader>o'] = { name = '[O]il', _ = 'which_key_ignore' },
+require('which-key').add {
+  { '<leader>c',   desc = '[C]ode and Copilot' },
+  { '<leader>cp',  desc = '[C]o[p]ilot' },
+  { '<leader>d',   desc = '[D]ocument' },
+  { '<leader>g',   desc = '[G]it' },
+  { '<leader>h',   desc = 'Git [H]unk and Harpoon' },
+  { '<leader>r',   desc = '[R]ename' },
+  { '<leader>s',   desc = '[S]earch' },
+  { '<leader>t',   desc = '[T]oggle' },
+  { '<leader>w',   desc = '[W]orkspace' },
+  { '<leader>o',   desc = '[O]il' },
 }
 -- register which-key VISUAL mode
 -- required for visual <leader>hs (hunk stage) to work
-require('which-key').register({
-  ['<leader>'] = { name = 'VISUAL <leader>' },
-  ['<leader>h'] = { 'Git [H]unk' },
+require('which-key').add({
+  {'<leader>' , name = 'VISUAL <leader>'},
+  {'<leader>h' , 'Git [H]unk'},
 }, { mode = 'v' })
 
 -- mason-lspconfig requires that these setup functions are called in this order
