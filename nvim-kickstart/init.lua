314fdf58fe787032130834786740a6f87eba3199
Anton Karlsson <anton.karlsson@fortnox.se>
2024-10-15 18:31:36 +0200

    feat: lazy load which-key.nvim plugin
    Modified the configuration to lazy load the which-key.nvim plugin using
    the "VeryLazy" event. This change improves startup performance by
    loading the plugin only when necessary.

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -116,7 +116,11 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim',  opts = {} },
+  {
+    'folke/which-key.nvim',
+    event = "VeryLazy",
+    opts = {}
+  },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
