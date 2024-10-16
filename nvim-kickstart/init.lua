5bc3f646f1ea550574d18a198e92ad3c0f65de5c
Anton Karlsson <anton.karlsson@fortnox.se>
2024-10-16 17:49:33 +0200

    Remove unused

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -789,13 +789,6 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
-
-  vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-    pattern = { "*.py" },
-    callback = function(ev)
-      -- vim.lsp.buf.format({ async = false })
-    end
-  })
 end
 
 -- document existing key chains
