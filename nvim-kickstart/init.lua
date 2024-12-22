23ffef334fbc2772d637d144941985bf0c3fa4b8
Anton Karlsson <anton93karlsson@gmail.com>
2024-12-22 19:13:25 +0100

    Use frag files as glsl

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -913,6 +913,11 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = "*.ibis",
   command = "set filetype=python"
 })
+-- Recognize .frag files as glsl files
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.frag",
+  command = "set filetype=glsl"
+})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
