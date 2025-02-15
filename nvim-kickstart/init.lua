20a63dc0f044626f7f147c437a94e1c4db68167b
Anton Karlsson <anton93karlsson@gmail.com>
2025-02-15 16:16:52 +0100

    Add c and glsl config for nvim

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -918,6 +918,10 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = "*.frag",
   command = "set filetype=glsl"
 })
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.vert",
+  command = "set filetype=glsl"
+})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
