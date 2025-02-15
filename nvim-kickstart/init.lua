3dc67204d1c39b79a7e5683ac6584440dfbbab78
Anton Karlsson <anton.karlsson@fortnox.se>
2025-02-15 18:59:12 +0100

    Add dadbod-ui

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -117,6 +117,25 @@ require('lazy').setup({
   },
 
   {
+    {
+      'kristijanhusak/vim-dadbod-ui',
+      dependencies = {
+        { 'tpope/vim-dadbod',                     lazy = true },
+        { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+      },
+      cmd = {
+        'DBUI',
+        'DBUIToggle',
+        'DBUIAddConnection',
+        'DBUIFindBuffer',
+      },
+      init = function()
+        -- Your DBUI configuration
+        vim.g.db_ui_use_nerd_fonts = 1
+      end,
+    },
+  },
+  {
     -- auto completion
     'saghen/blink.cmp',
     -- optional: provides snippets for the snippet source
