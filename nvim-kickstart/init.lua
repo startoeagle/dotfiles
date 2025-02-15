ad0bf1da3da093b5e956f813c932e80dee7ab74d
Anton Karlsson <anton.karlsson@fortnox.se>
2025-02-15 19:01:48 +0100

    Add new line after format

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -948,3 +948,4 @@ nmap("[q", function() vim.cmd("cprevious") end, { desc = 'Previous quick fix ite
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
