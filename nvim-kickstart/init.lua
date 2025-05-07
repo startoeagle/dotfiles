b06150ced0d1ee441a4442c64b81cbfaded4f6d7
Anton Karlsson <anton.karlsson@fortnox.se>
2025-05-07 08:54:21 +0200

    Add haskell-tools

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -117,6 +117,12 @@ require('lazy').setup({
   },
 
   {
+    'mrcjkb/haskell-tools.nvim',
+    version = '^5', -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
+
+  {
     {
       'kristijanhusak/vim-dadbod-ui',
       dependencies = {
