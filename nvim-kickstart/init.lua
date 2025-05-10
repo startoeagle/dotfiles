dc562032cc3d65f579f52309990203464078d70d
Anton Karlsson <anton.karlsson@fortnox.se>
2025-05-10 21:51:19 +0200

    Add more servers

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -545,18 +545,24 @@ require('lazy').setup {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
+        clangd = {},
+        gopls = {},
+        basedpyright = {
+          python = {
+            analysis = {
+              typeCheckingMode = 'on',
+            },
+          },
+        },
+
+        rust_analyzer = {},
+        tsserver = {},
+        html = { filetypes = { 'html', 'twig', 'hbs' } },
+        kotlin_language_server = {
+          jvm = {
+            target = '21',
+          },
+        },
 
         lua_ls = {
           -- cmd = { ... },
