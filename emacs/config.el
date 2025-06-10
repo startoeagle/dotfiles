95713b4e3518cff4d3b7021283cf1f94f39be7bd
Anton Karlsson <anton.karlsson@fortnox.se>
2025-06-10 19:01:39 +0200

    Add slides functionality to org

--- emacs/config.el
+++ emacs/config.el
@@ -97,3 +97,11 @@
       (java . t)
       (php . t)
       (shell . t)))
+
+(use-package org-tree-slide) 
+  (define-key org-mode-map (kbd "<f8>") 'org-tree-slide-mode)
+  (define-key org-mode-map (kbd "S-<f8>") 'org-tree-slide-skip-done-toggle)
+(with-eval-after-load "org-tree-slide"
+  (define-key org-tree-slide-mode-map (kbd "<f9>") 'org-tree-slide-move-previous-tree)
+  (define-key org-tree-slide-mode-map (kbd "<f10>") 'org-tree-slide-move-next-tree)
+  )
