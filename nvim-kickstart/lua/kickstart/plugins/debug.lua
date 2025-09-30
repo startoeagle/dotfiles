5a95ef365709a3ee2473c2023426f586d2f45e1a
Anton Karlsson <anton.karlsson@fortnox.se>
2025-09-30 18:03:09 +0200

    Update debug keymap

--- nvim-kickstart/lua/kickstart/plugins/debug.lua
+++ nvim-kickstart/lua/kickstart/plugins/debug.lua
@@ -27,28 +27,28 @@ return {
   keys = {
     -- Basic debugging keymaps, feel free to change to your liking!
     {
-      '<F5>',
+      '<leader>dc',
       function()
         require('dap').continue()
       end,
       desc = 'Debug: Start/Continue',
     },
     {
-      '<F1>',
+      '<leader>di',
       function()
         require('dap').step_into()
       end,
       desc = 'Debug: Step Into',
     },
     {
-      '<F2>',
+      '<c-s>',
       function()
         require('dap').step_over()
       end,
       desc = 'Debug: Step Over',
     },
     {
-      '<F3>',
+      '<leader>do',
       function()
         require('dap').step_out()
       end,
