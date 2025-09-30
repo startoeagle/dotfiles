f2320dca84e703ff994209298bc1fbb10dddd120
Anton Karlsson <anton.karlsson@fortnox.se>
2025-09-30 18:02:52 +0200

    Add nvim-dap-lldb

--- nvim-kickstart/lua/custom/plugins/init.lua
+++ nvim-kickstart/lua/custom/plugins/init.lua
@@ -184,4 +184,9 @@ return {
       vim.g.db_ui_use_nerd_fonts = 1
     end,
   },
+  {
+    'julianolf/nvim-dap-lldb',
+    dependencies = { 'mfussenegger/nvim-dap' },
+    opts = { codelldb_path = '/path/to/codelldb' },
+  },
 }
