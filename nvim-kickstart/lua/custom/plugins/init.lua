29e8629bce06ff810c00ece669be7f2dff0e5634
Anton Karlsson <anton.karlsson@fortnox.se>
2025-10-17 11:19:52 +0200

    Remove nvim-dap-lldb

--- nvim-kickstart/lua/custom/plugins/init.lua
+++ nvim-kickstart/lua/custom/plugins/init.lua
@@ -184,9 +184,4 @@ return {
       vim.g.db_ui_use_nerd_fonts = 1
     end,
   },
-  {
-    'julianolf/nvim-dap-lldb',
-    dependencies = { 'mfussenegger/nvim-dap' },
-    opts = { codelldb_path = '/path/to/codelldb' },
-  },
 }
