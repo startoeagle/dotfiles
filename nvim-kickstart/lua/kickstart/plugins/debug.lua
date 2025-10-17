82d7ebcfac28b1c4d07758295ff5b5449f55531d
Anton Karlsson <anton.karlsson@fortnox.se>
2025-10-17 11:30:37 +0200

    Revamp debug

--- nvim-kickstart/lua/kickstart/plugins/debug.lua
+++ nvim-kickstart/lua/kickstart/plugins/debug.lua
@@ -11,12 +11,12 @@ return {
   'mfussenegger/nvim-dap',
   -- NOTE: And you can specify dependencies as well
   dependencies = {
-    -- Creates a beautiful debugger UI
-    'rcarriga/nvim-dap-ui',
-
-    -- Required dependency for nvim-dap-ui
-    'nvim-neotest/nvim-nio',
-
+    {
+      'igorlfs/nvim-dap-view',
+      ---@module 'dap-view'
+      ---@type dapview.Config
+      opts = {},
+    },
     -- Installs the debug adapters for you
     'williamboman/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
@@ -69,17 +69,9 @@ return {
       desc = 'Debug: Set Breakpoint',
     },
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-    {
-      '<F7>',
-      function()
-        require('dapui').toggle()
-      end,
-      desc = 'Debug: See last session result.',
-    },
   },
   config = function()
     local dap = require 'dap'
-    local dapui = require 'dapui'
 
     require('mason-nvim-dap').setup {
       -- Makes a best effort to setup the various debuggers with
@@ -98,40 +90,34 @@ return {
       },
     }
 
-    -- Dap UI setup
-    -- For more information, see |:help nvim-dap-ui|
-    dapui.setup {
-      -- Set icons to characters that are more likely to work in every terminal.
-      --    Feel free to remove or use ones that you like more! :)
-      --    Don't feel like these are good choices.
-      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-      controls = {
-        icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
-        },
-      },
-    }
-
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
-
-    vim.keymap.set('v', '<leader>ee', dapui.eval)
-
-    -- Install golang specific config
     require('dap-go').setup {
       delve = {
         -- On Windows delve must be run attached or it crashes.
         -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
         detached = vim.fn.has 'win32' == 0,
+      },
+    }
+
+    dap.adapters.lldb = {
+      type = 'server',
+      port = '${port}',
+      executable = {
+        command = '/Users/anton.karlsson/.local/share/nvim/mason/packages/codelldb/codelldb',
+        args = { '--port', '${port}' },
+        detached = vim.loop.os_uname().sysname ~= 'Windows',
+      },
+    }
+
+    print 'register python daps'
+    dap.configurations.python = {
+      {
+        name = 'Run pytest with lldb',
+        type = 'lldb',
+        request = 'launch',
+        program = 'python',
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+        args = { '-m', 'pytest', vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()) },
       },
     }
   end,
