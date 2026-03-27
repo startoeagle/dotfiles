afa39ff72ae8a363366875ba820446c2f35f77bb
Anton Karlsson <anton.karlsson@fortnox.se>
2026-03-27 17:05:03 +0100

    Fix todo

--- nvim_new/ftplugin/python.lua
+++ nvim_new/ftplugin/python.lua
@@ -2,3 +2,10 @@ local set = vim.keymap.set
 
 set('n', '<leader>kr', function() vim.cmd 'edit term://uv run python %' end, {desc = 'Run current file with uv'})
 
+
+
+require('debugmaster').keys.add({
+    key = "<CR>",
+    action = require('dap-python').test_method,
+    desc = "Jump to the current stack frame"
+})
