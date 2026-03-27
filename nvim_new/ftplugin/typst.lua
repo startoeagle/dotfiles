75e84f8e894df0ccf85c4c4c97454f0037985d64
Anton Karlsson <anton.karlsson@fortnox.se>
2026-03-27 16:47:57 +0100

    Refactor

--- nvim_new/ftplugin/typst.lua
+++ nvim_new/ftplugin/typst.lua
@@ -3,5 +3,5 @@ local set = vim.keymap.set
 set('i', '$$', '$$<esc>i')
 set('i', '$ ', '$  $<esc><left>i')
 
-vim.opt.spell=true
-vim.opt.spelllang="en_us"
+vim.opt_local.spell=true
+vim.opt_local.spelllang="en_us"
