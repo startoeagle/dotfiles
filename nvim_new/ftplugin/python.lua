b0af19e679711b16acb179a726f260fea06527d9
Anton Karlsson <anton.karlsson@fortnox.se>
2026-03-27 17:06:15 +0100

    Format

--- nvim_new/ftplugin/python.lua
+++ nvim_new/ftplugin/python.lua
@@ -1,6 +1,6 @@
 local set = vim.keymap.set
 
-set('n', '<leader>kr', function() vim.cmd 'edit term://uv run python %' end, {desc = 'Run current file with uv'})
+set('n', '<leader>kr', function() vim.cmd 'edit term://uv run python %' end, { desc = 'Run current file with uv' })
 
 
 
