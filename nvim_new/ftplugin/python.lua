cc846d5d4a1260771062ab3a27d142052e6617ab
Anton Karlsson <anton.karlsson@fortnox.se>
2026-04-02 16:54:51 +0200

    Add fucntion to delete all test that has been started with pytest

--- nvim_new/ftplugin/python.lua
+++ nvim_new/ftplugin/python.lua
@@ -1,7 +1,19 @@
 local set = vim.keymap.set
 
 set('n', '<leader>kr', function() vim.cmd 'edit term://uv run python %' end, { desc = 'Run current file with uv' })
-set('n', '<leader>kt', function() vim.cmd 'edit term://uv run pytest -vv %' end, { desc = 'Run current file with pytest via uv' })
+set('n', '<leader>kt', function() vim.cmd 'edit term://uv run pytest -vv %' end,
+    { desc = 'Run current file with pytest via uv' })
+
+set('n', '<leader>kdt', function()
+    local bufs = vim.api.nvim_list_bufs()
+    local bufs_to_delete = vim.tbl_filter(function(b)
+        local name = vim.api.nvim_buf_get_name(b)
+        return name:find('pytest') ~= nil
+    end, bufs)
+    for _, b in ipairs(bufs_to_delete) do
+        vim.api.nvim_buf_delete(b, {})
+    end
+end, { desc = 'Delete ALL buffers that have been created with pytest via uv' })
 
 require('debugmaster').keys.add({
     key = "<CR>",
