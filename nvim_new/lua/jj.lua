fd46eac45be13e0d615df6ad5418f9db426f3003
Anton Karlsson <anton.karlsson@fortnox.se>
2026-04-07 14:50:56 +0200

    Add error handling

--- nvim_new/lua/jj.lua
+++ nvim_new/lua/jj.lua
@@ -7,7 +7,12 @@ M.open = function()
         vim.api.nvim_set_current_buf(M.jjbuff)
         vim.cmd('edit term://jjui')
     else
-        vim.api.nvim_set_current_buf(M.jjbuff)
+        local res = pcall(function() vim.api.nvim_set_current_buf(M.jjbuff) end)
+        if not res then
+            M.jjbuff = vim.api.nvim_create_buf(true, false)
+            vim.api.nvim_set_current_buf(M.jjbuff)
+            vim.cmd('edit term://jjui')
+        end
     end
 end
 
